a5e9a7fb56fb611d2468620eb46c971fe4a1f417
MelsCrest <melisacresta@gmail.com>
2025-03-26 17:47:04 +0100

    menu responsive

--- web/src/components/Header.jsx
+++ web/src/components/Header.jsx
@@ -4,13 +4,13 @@ import MainMenu from './MainMenu'
 
 function Header() {
   return (
-    <header>
-      <h1 className='text-lg font-bold'>
+    <header className='container mx-auto flex flex-row px-5 py-7'>
+      <h1 className='text-lg font-bold bg-red-500 flex-1 pt-1'>
         <FontAwesomeIcon icon={faBolt} />
         Blog de TechTronic
       </h1>
-      <MainMenu/>
-      <button type="button" className='text-white bg-black rounded-lg py-2 px-4'>Tienda</button>
+      <MainMenu />
+      <button type="button" className='text-white bg-black rounded-lg py-2 px-4 flex 1'>Tienda</button>
     </header>
   )
 }
--- web/src/components/MainMenu.jsx
+++ web/src/components/MainMenu.jsx
@@ -1,13 +1,18 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 function MainMenu() {
   return (
-    <nav>
-      <ul>
-        <li>Inicio</li>
-        <li>Blog</li>
-        <li>Productos</li>
-        <li>Sobre nosotros</li>
-        <li>Contacto</li>
+    <nav className="bg-green-300 pr-12 relative">
+      <div className='block sm:hidden absolute right-4 top-2'>
+        <FontAwesomeIcon icon={faBars}/>
+      </div>
+      <ul className="hidden sm:flex flex-row gap-12 pt-2">
+        <li className="hover:text-lg hover:font-bold hover:underline underline-offset-2">Inicio</li>
+        <li className="hover:text-lg hover:font-bold hover:underline underline-offset-2">Blog</li>
+        <li className="hover:text-lg hover:font-bold hover:underline underline-offset-2">Productos</li>
+        <li className="hover:text-lg hover:font-bold hover:underline underline-offset-2">Sobre nosotros</li>
+        <li className="hover:text-lg hover:font-bold hover:underline underline-offset-2">Contacto</li>
       </ul>
     </nav>
   )
