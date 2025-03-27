335e3a7b34d7af07e8298ff1f9b0a5faf9e63c05
MelsCrest <melisacresta@gmail.com>
2025-03-27 12:30:39 +0100

    drop-down menu funtionality

--- web/src/components/Header.jsx
+++ web/src/components/Header.jsx
@@ -5,12 +5,12 @@ import MainMenu from './MainMenu'
 function Header() {
   return (
     <header className='container mx-auto flex flex-row px-5 py-7'>
-      <h1 className='text-lg font-bold bg-red-500 flex-1 pt-1'>
+      <h1 className='text-lg font-bold bg-red-500 flex-1 pt-1 -order-2 md:order-0'>
         <FontAwesomeIcon icon={faBolt} />
         Blog de TechTronic
       </h1>
       <MainMenu />
-      <button type="button" className='text-white bg-black rounded-lg py-2 px-4 flex 1'>Tienda</button>
+      <button type="button" className='text-white bg-black rounded-lg py-2 px-4 flex 1 -order-1 md:order-0'>Tienda</button>
     </header>
   )
 }
--- web/src/components/MainMenu.jsx
+++ web/src/components/MainMenu.jsx
@@ -1,18 +1,24 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 function MainMenu() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const handleClick = ()=>{
+    setIsMenuOpen(!isMenuOpen);
+  }
+
   return (
     <nav className="bg-green-300 pr-12 relative">
-      <div className='block sm:hidden absolute right-4 top-2'>
+      <div className='block md:hidden absolute right-4 top-2' onClick={handleClick}>
         <FontAwesomeIcon icon={faBars}/>
       </div>
-      <ul className="hidden sm:flex flex-row gap-12 pt-2">
-        <li className="hover:text-lg hover:font-bold hover:underline underline-offset-2">Inicio</li>
-        <li className="hover:text-lg hover:font-bold hover:underline underline-offset-2">Blog</li>
-        <li className="hover:text-lg hover:font-bold hover:underline underline-offset-2">Productos</li>
-        <li className="hover:text-lg hover:font-bold hover:underline underline-offset-2">Sobre nosotros</li>
-        <li className="hover:text-lg hover:font-bold hover:underline underline-offset-2">Contacto</li>
+      <ul className={`flex flex-col items-end gap-6 text-right absolute top-10 right-0 md:flex md:flex-row md:gap-12 md:static pt-2 ${isMenuOpen ? 'block' : 'hidden'} md:block`}>
+        <li className="hover:underline underline-offset-2">Inicio</li>
+        <li className="hover:underline underline-offset-2">Blog</li>
+        <li className="hover:underline underline-offset-2">Productos</li>
+        <li className="hover:underline underline-offset-2">Sobre nosotros</li>
+        <li className="hover:underline underline-offset-2">Contacto</li>
       </ul>
     </nav>
   )
