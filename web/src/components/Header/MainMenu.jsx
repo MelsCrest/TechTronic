4d9614d7d8685249901c31d33a29aa6e5c90788f
MelsCrest <melisacresta@gmail.com>
2025-03-31 10:50:10 +0200

    bug

--- web/src/components/Header/MainMenu.jsx
+++ web/src/components/Header/MainMenu.jsx
@@ -14,7 +14,7 @@ function MainMenu() {
       <div className='block md:hidden absolute right-4 top-2' onClick={handleClick}>
         <FontAwesomeIcon icon={faBars}/>
       </div>
-      <ul className={`flex flex-col items-end gap-6 text-right absolute top-10 right-0 md:flex md:flex-row md:gap-12 md:static pt-2 ${isMenuOpen ? 'block' : 'hidden'} md:block`}>
+      <ul className={`flex flex-col items-end gap-6 text-right absolute top-10 right-0 md:flex md:flex-row md:gap-12 md:static pt-2 ${isMenuOpen ? 'block' : 'hidden'}`}>
         <li className="hover:underline underline-offset-2">Inicio</li>
         <li className="hover:underline underline-offset-2">Blog</li>
         <li className="hover:underline underline-offset-2">Productos</li>
